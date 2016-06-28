a40067d14f9419a0cebc3dcacd7fe912269a05c4
Michael Henry <michaelhenry91@gmail.com>
2016-06-27 20:17:48 -0400

    made all of the tests pass

--- Gemfile
+++ Gemfile
@@ -2,6 +2,7 @@ source 'https://rubygems.org'
 
 gem 'sinatra'
 gem 'cat_api'
+gem 'strftime'
 
 group :test do
   gem 'rspec'
--- helpers/methods_practice_helpers.rb
+++ helpers/methods_practice_helpers.rb
@@ -2,18 +2,23 @@ module MethodsPracticeHelpers
 
   def image_tag(source, alternate_text)
     # Generate an image tag with the given source and the classes
+    %(
+      <img class="img-responsive img-thumbnail" src="#{source}" alt="#{alternate_text}" height="42" width="42">
+    )
     # "img-responsive" and "img-thumbnail". The image should also have an alt
-    # attribute with the contents of alternate_text.
+    # attribute with the contents of alternate_text..
   end
 
   def bootstrap_alert(message, type='info')
-    # Generates the HTML for a Bootstrap alert (useful since it can be diffcult
-    # to remember all HTML needed to do it right). It should show the message in
-    # the "message" variable and should have a class appropriate for the "type"
-    # that is passed in, defaulting to 'info' if no type is passed in.
+    %(
+    <div class="alert alert-#{type} alert-dismissible" role="alert">
+      <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+      #{message}
+    </div>    )
   end
 
   def current_date_and_time
+    Time.now.strftime("%B %-d, %Y at %-l:%M%P")
     # Give the current date and time, in the format: "February 6, 2015 at 4:25pm"
     # Time.now.strftime is a great method chain you can use for this purpose.
     # See how to use it at: http://apidock.com/ruby/Time/strftime
